216252b3c4f653f91e1a7d3a46e3f86795749fc5
Jan Nikolai Trzeszkowski <jan.nikolai.trzeszkowski@innoq.com>
2019-10-24 17:43:09 +0200

    Assignments for Admin

--- missions/admin.py
+++ missions/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ship, Mission
+from .models import Ship, Mission, Assignment
+
+class AssignmentInline(admin.TabularInline):
+    model = Assignment
+
+class MissionAdmin(admin.ModelAdmin):
+    inlines = [AssignmentInline]
 
 admin.site.register(Ship)
-admin.site.register(Mission)
+admin.site.register(Mission, MissionAdmin)
+admin.site.register(Assignment)
