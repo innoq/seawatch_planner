029ba0969fa46a049bd81fc3b8a76b16a516bb10
Martin Otten <martin.otten@grandcentrix.net>
2019-10-25 14:20:53 +0200

    Start of mission workflow

---
+++ missions/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Assignment
+
+class AssignmentForm(forms.ModelForm):
+    class Meta:
+        model = Assignment
+        fields = ['position']
--- missions/urls.py
+++ missions/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('edit_mission', views.show_mission_editor, name='edit_mission'),
+    path('', views.MissionListView.as_view(), name='mission-list'),
+    path('new', views.MissionCreateView.as_view(), name='mission-create'),
+    path('<int:pk>/', views.MissionDetailView.as_view(), name='mission-detail'),
+    path('<int:mission__id>/assignments/new', views.AssignmentNewView.as_view(), name="assignment-create"),
+    path('<int:mission__id>/assignments/<int:assignment__id>', views.AssignmentView.as_view(), name="assignment"),
+    path('<int:mission__id>/assignments/<int:assignment__id>/assignee', views.AssigneeView.as_view(), name="assignee"),
 ]
