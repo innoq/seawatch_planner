c13e4c2ef59632013ae765af43a54eb6e0c13e16
Joshua Toepfer <joshua.toepfer@innoq.com>
2019-11-14 16:56:58 +0100

    added test base

--- seawatch_registration/tests/views/test_requested_positions_view.py
+++ seawatch_registration/tests/views/test_requested_positions_view.py
@@ -1,46 +1,22 @@
-from django.contrib.auth.models import User
-from django.test import TestCase, Client
 from django.urls import reverse
 
 from seawatch_registration.models import Profile, Position
-from seawatch_registration.tests.views import util
+from seawatch_registration.tests.views.test_base import TestBase
 
 
-class TestAddRequestedPositionsView(TestCase):
+class TestAddRequestedPositionsView(TestBase):
 
     def setUp(self) -> None:
-        self.client = Client()
-        self.username = 'testuser1'
-        self.password = '1X<ISRUkw+tuK'
-        self.user = User.objects.create_user(username=self.username, password=self.password)
-        self.user.save()
-        self.url_add_positions = reverse('add_requested_profile')
-
-    def test_views__add_requested_positions__get__should_redirect_to_login_when_not_logged_in(self):
-        # Act
-        response = self.client.get(self.url_add_positions, user=self.user)
-
-        # Assert
-        self.assertRedirects(response, '/accounts/login/?next=/accounts/position/')
-
-    def test_views__add_requested_positions__get__should_get_403_when_profile_does_not_exist(self):
-        # Arrange
-        self.client.login(username=self.username, password=self.password)
-
-        # Act
-        response = self.client.get(self.url_add_positions, user=self.user)
-
-        # Assert
-        self.assertEquals(response.status_code, 403)
+        self.base_set_up(url=reverse('add_requested_profile'), login_required=True, profile_required=True)
 
     def test_views__add_requested_positions__get__should_render_with_form_html_when_profile_exists(self):
         # Arrange
-        profile: Profile = util.get_profile(self.user)
+        profile: Profile = self.profile
         profile.save()
         self.client.login(username=self.username, password=self.password)
 
         # Act
-        response = self.client.get(self.url_add_positions, user=self.user)
+        response = self.client.get(self.url, user=self.user)
 
         # Assert
         self.assertEquals(response.status_code, 200)
@@ -48,13 +24,13 @@ class TestAddRequestedPositionsView(TestCase):
 
     def test_views__add_requested_positions__get__should_show_selected_positions_when_requested_positions_exists(self):
         # Arrange
-        profile: Profile = util.get_profile(self.user)
+        profile: Profile = self.profile
         profile.requested_positions.add(Position.objects.filter().first())
         profile.save()
         self.client.login(username=self.username, password=self.password)
 
         # Act
-        response = self.client.get(self.url_add_positions, user=self.user)
+        response = self.client.get(self.url, user=self.user)
 
         # Assert
         self.assertEquals(response.status_code, 200)
@@ -64,13 +40,13 @@ class TestAddRequestedPositionsView(TestCase):
     def test_views__add_requested_positions__post__should_render_error_when_no_position_is_selected(self):
         # Arrange
         self.client.login(username=self.username, password=self.password)
-        profile: Profile = util.get_profile(self.user)
+        profile: Profile = self.profile
         position = Position.objects.filter().first()
         profile.requested_positions.add(position)
         profile.save()
 
         # Act
-        response = self.client.post(self.url_add_positions,
+        response = self.client.post(self.url,
                                     {},
                                     user=self.user)
 
@@ -83,12 +59,12 @@ class TestAddRequestedPositionsView(TestCase):
     def test_views__add_requested_position__post__should_render_success_when_2_positions_are_set(self):
         # Arrange
         self.client.login(username=self.username, password=self.password)
-        profile: Profile = util.get_profile(self.user)
+        profile: Profile = self.profile
         position = Position.objects.filter().first()
         profile.save()
 
         # Act
-        response = self.client.post(self.url_add_positions,
+        response = self.client.post(self.url,
                                     {'requested_positions': position.id},
                                     user=self.user)
 
