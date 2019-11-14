47024545949a122c002d0daeb5e87769a726097b
Joshua Toepfer <joshua.toepfer@innoq.com>
2019-11-14 09:35:24 +0100

    added additional tests for questions view

--- seawatch_registration/Tests/test_views.py
+++ seawatch_registration/Tests/test_views.py
@@ -535,6 +535,41 @@ class TestViews(TestCase):
         self.assertEquals(Answer.objects.filter().count(), 1)
         self.assertEquals(Answer.objects.filter().first().text, 'Answer Required')
 
+    def test_views__questions__post__should_not_update_answer_when_question_is_mandatory_and_answer_is_blank(self):
+        # Arrange
+        profile: Profile = self.get_profile()
+        profile.save()
+        question = Question.objects.filter().first()
+        answer = Answer(text="Example Answer", profile=profile, question=question)
+        answer.save()
+
+        self.client.login(username=self.username, password=self.password)
+
+        # Act
+        response = self.client.post(self.url_questions, {}, user=self.user)
+
+        # Assert
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'form.html')
+        self.assertEquals(Answer.objects.filter().count(), 1)
+        self.assertEquals(Answer.objects.filter().first().text, 'Example Answer')
+
+    def test_views__questions__post__should_not_add_answer_when_question_is_mandatory_and_answer_is_blank(self):
+        # Arrange
+        profile: Profile = self.get_profile()
+        profile.save()
+        question = Question.objects.filter().first()
+
+        self.client.login(username=self.username, password=self.password)
+
+        # Act
+        response = self.client.post(self.url_questions, {}, user=self.user)
+
+        # Assert
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'form.html')
+        self.assertEquals(Answer.objects.filter().count(), 0)
+
     def test_views__questions__post__should_render_success_when_question_is_answered(self):
         # Arrange
         self.client.login(username=self.username, password=self.password)
