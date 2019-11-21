f2fdfb77f5778f072755fb72dcd75851737842ba
Joshua Toepfer <joshua.toepfer@innoq.com>
2019-11-21 08:54:50 +0100

    fixed django heroku settings

--- seawatch_planner/settings.py
+++ seawatch_planner/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -134,3 +132,6 @@ NOSE_ARGS = ['--nocapture',
              '--nologcapture']
 
 MEDIA_ROOT = 'media/'
+
+# Heroku Deploy
+django_heroku.settings(locals())
