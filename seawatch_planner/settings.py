2188e4891af3749507e32aeb08f08779bc59a5b3
Joshua Toepfer <joshua.toepfer@innoq.com>
2019-11-22 18:23:40 +0100

    changed email config for dev purpose

--- seawatch_planner/settings.py
+++ seawatch_planner/settings.py
@@ -132,3 +132,7 @@ NOSE_ARGS = ['--nocapture',
              '--nologcapture']
 
 MEDIA_ROOT = 'media/'
+
+# Email Config
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
