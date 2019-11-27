0286bbb984622c07562d98c4db0f42fd1e249eb0
Michael Schwarz <michael.schwarz@innoq.com>
2019-11-27 14:25:17 +0100

    add availability to navbar

--- seawatch_registration/views/availability_view.py
+++ seawatch_registration/views/availability_view.py
@@ -7,48 +7,38 @@ from seawatch_registration.forms.availability_form import AvailableDatesFormset
 
 
 class AvailabilityView(LoginRequiredMixin, UserPassesTestMixin, View):
-    
-    def __init__(self):
-        super(AvailabilityView, self).__init__()
-        self.title = 'Availability'
-        self.success_alert = 'Available Dates successfully saved!'
-        self.submit_button = 'Next'
+
+    nav_item = 'availability'
+    title = 'Add Availability'
+    success_alert = 'Available Dates successfully saved!'
+    submit_button = 'Next'
+    template_name = 'availability.html'
 
     def get(self, request, *args, **kwargs):
         profile = Profile.objects.get(user=request.user)
         formset = AvailableDatesFormset(instance=profile)
-        return render(request,
-                        'availability.html',
+        return render(request, self.template_name,
                         {'formset': formset,
-                        'title': self.title,
-                        'success_alert': self.success_alert,
-                        'submit_button': self.submit_button
+                        'view': self
                         })
 
     def post(self, request, *args, **kwargs):
         profile = Profile.objects.get(user=request.user)
         formset = AvailableDatesFormset(request.POST, instance=profile)
         if not formset.is_valid():      
-            return render(request,
-                            'availability.html',
+            return render(request, self.template_name,
                             {'formset': formset,
+                            'view': self,
                             'error': formset.errors,
-                            'title': self.title,
-                            'success_alert': self.success_alert,
-                            'submit_button': self.submit_button
                             })
 
         formset.save()
         #return redirect('show_profile')
 
         formset = AvailableDatesFormset(instance=profile)
-        return render(request,
-                        'availability.html',
+        return render(request, self.template_name,
                         {'formset': formset,
-                        'title': self.title,
-                        'success': True,
-                        'success_alert': self.success_alert,
-                        'submit_button': self.submit_button
+                        'view': self
                         })
 
     def test_func(self):
