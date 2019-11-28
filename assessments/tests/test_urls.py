434f0bb9e48b597dd841268d97a92860baea37c0
Joshua Toepfer <joshua.toepfer@innoq.com>
2019-11-28 13:14:12 +0100

    seperated views in assessment

--- assessments/tests/test_urls.py
+++ assessments/tests/test_urls.py
@@ -1,8 +1,7 @@
 from django.test import SimpleTestCase, Client
 from django.urls import resolve, reverse
 
-from assessments.views.assessment_list_view import AssessmentListView
-from assessments.views.assessment_view import AssessmentUpdateView
+import assessments.views.assessment as assessment
 
 
 class TestUrls(SimpleTestCase):
@@ -12,8 +11,8 @@ class TestUrls(SimpleTestCase):
 
     def test_urls_assessment_list(self):
         url = reverse('assessment_list')
-        self.assertEquals(resolve(url).func.__name__, AssessmentListView.as_view().__name__)
+        self.assertEquals(resolve(url).func.__name__, assessment.ListView.as_view().__name__)
 
     def test_urls_assessment(self):
         url = reverse('assessment_update', kwargs={'profile_id': 1})
-        self.assertEquals(resolve(url).func.__name__, AssessmentUpdateView.as_view().__name__)
+        self.assertEquals(resolve(url).func.__name__, assessment.UpdateView.as_view().__name__)
--- assessments/urls.py
+++ assessments/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from assessments.views.assessment_list_view import AssessmentListView
-from assessments.views.assessment_view import AssessmentUpdateView
+import assessments.views.assessment as assessment
 
 urlpatterns = [
-    path('', AssessmentListView.as_view(), name='assessment_list'),
-    path('<int:profile_id>/edit', AssessmentUpdateView.as_view(), name='assessment_update'),
+    path('', assessment.ListView.as_view(), name='assessment_list'),
+    path('<int:profile_id>/edit', assessment.UpdateView.as_view(), name='assessment_update'),
 ]
