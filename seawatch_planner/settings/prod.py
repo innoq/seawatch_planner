89c35f0404e2bfd92c48a64611c8457f9960ab83
Joshua Toepfer <joshua.toepfer@innoq.com>
2019-12-17 12:41:09 +0100

    changed email backend for prod

--- seawatch_planner/settings/prod.py
+++ seawatch_planner/settings/prod.py
@@ -1,5 +1,3 @@
-import os
-
 import django_heroku
 
 from .base import *
@@ -10,8 +8,12 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY_DJANGO']
 
 # Email Config
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 ## Heroku Deploy
 django_heroku.settings(locals())
